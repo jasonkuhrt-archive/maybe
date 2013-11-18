46a3b90742b3439e7d7999ecac059558e591a80b
Jason Kuhrt <jasonkuhrt@me.com>
2013-11-18 12:03:11 -0500

    Add Functor support like a Monad should have - closes #4

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ var api = require('./api');
 
 // Helpers
 
+var from_maybe = api.from_maybe;
+var from_just = api.from_just;
+var is_just = api.is_just;
+var is_nothing = api.is_nothing;
+
 var partial_right = _.partialRight;
 var merge = _.merge;
 var extend = _.extend;
 var transform = _.transform;
 
-function is_nothing(maybe_something){
-  return maybe_something === undefined || maybe_something === null
+function exists(maybe_something){
+  return maybe_something !== undefined && maybe_something !== null
 }
 
 function map_object(obj, f){
@@ -26,12 +31,17 @@ function map_object(obj, f){
 var Maybe = module.exports = MONAD(function(monad, value){
   // Private fields are used by Maybe.* functions
   monad._value = value;
-  monad._is_nothing = is_nothing(value);
+  monad._is_nothing = !exists(value);
+
+  // Maybe has a map implementation because it is a Monad and all Monads are Functors.
+  monad.map = function(func){
+    return is_just(monad) ? Maybe(func(from_just(monad))) : monad ;
+  }
 
   // When Maybe's Just a is Nothing
   // Then Maybe.bind simply returns the monad
   // without applying the passed function
-  if (Maybe.is_nothing(monad)){
+  if (is_nothing(monad)){
     monad.bind = function(ignored_function){
       return monad;
     };
@@ -39,7 +49,6 @@ var Maybe = module.exports = MONAD(function(monad, value){
 
   // Merge a partially-applied copy of the API
   merge(monad, map_object(api, function(f, fname){ return partial_right(f, monad); }));
-
 });
 
 
--- test/index.js
+++ test/index.js
@@ -1,10 +1,27 @@
+var curry = require('lodash').curry;
 var Maybe = require('../');
+
+var l = console.log;
+var add = curry(function(a, b){ return a + b });
+var map = curry(function(func, collection){
+  return collection.map(func);
+});
+
+
+
 
 // Psuedo Tests
 
-console.log(Maybe.is_nothing(Maybe(2)));
-console.log(Maybe.is_just(Maybe(false)));
-console.log(Maybe.from_maybe('foo', Maybe(2)), Maybe.from_maybe(55, Maybe(null)));
-console.log(Maybe(2).from_maybe('foobar'), Maybe(null).from_maybe('foobar'))
-Maybe(null).maybe(4, console.log);
-Maybe.maybe(4, console.log, Maybe(null));
+l(Maybe.is_nothing(Maybe(2)));
+l(Maybe.is_just(Maybe(false)));
+l(Maybe.from_maybe('foo', Maybe(2)), Maybe.from_maybe(55, Maybe(null)));
+l(Maybe(2).from_maybe('foobar'), Maybe(null).from_maybe('foobar'))
+
+Maybe(null).maybe(4, l);
+Maybe.maybe(4, l, Maybe(null));
+
+
+// Trying map
+
+l(map(add(1), Maybe(5)));
+l(map(add(1), Maybe()).is_nothing());
